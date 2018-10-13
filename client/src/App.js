dfabc3f079ec7439538a8ab531f9cdbdfcc5da18
e-be-walk <e.be.walk@gmail.com>
2018-10-13 10:24:34 -0400

    react routes and axios for image handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, HashRouter as Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import SiteIndex from './Site/Index';
+import SiteNew from './Site/New';
+import SiteEdit from './Site/Edit';
+
 
 class App extends Component {
   render() {
+    /*will need a way to check users/authentication*/
+
+    /* the following route should be used for the overall site and should probably not be within the sites routes- need to fix*/
+    const siteRoutes = (
+      <div>
+      <Route path="/" component={ showHeader } />
+      <Route path="/" component={ showMain } />
+      <Route path="/sites/:id" component={Site} />
+      <Route path="/sites/:id/edit" component={SiteEdit} />
+      <Route path="/sites/new" component={SiteNew} />
+      <Route path="/sites/" component={SiteIndex} />
+      </div>
+    )
+    
     return (
       <div className="App">
         <header className="App-header">
